895e7c24c7d81d1530eadd73f39b29dce917ff81
Anna Nitsevich <dilmun94@gmail.com>
2025-09-06 21:51:18 +0500

    feat: add solutions for tasks 200-270 strings

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -197,8 +197,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let sumOfChars = 0;
+  if (typeof str !== 'string' || !str.length) {
+    return 0;
+  }
+  for (let i = 0; i < str.length; i += 1) {
+    sumOfChars += str.charCodeAt(i);
+  }
+  return sumOfChars;
 }
 
 /**
@@ -212,8 +219,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -227,8 +234,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -244,8 +251,10 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const formatMinutes = `${minutes}`.padStart(2, '0');
+  const formatSeconds = `${seconds}`.padStart(2, '0');
+  return `${formatMinutes}:${formatSeconds}`;
 }
 
 /**
@@ -258,8 +267,12 @@ function formatTime(/* minutes, seconds */) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  let reverseStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
 }
 
 /**
